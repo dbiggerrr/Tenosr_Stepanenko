21fa63c4d709fa41a733f730d2a09a2ad69cef00
dbiggerrr <51165090+dbiggerrr@users.noreply.github.com>
2019-12-07 15:42:54 +0200

    Update main.cpp
    main file with included test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Tensor_h.h"
+#include "TensorFinal_h.h"
 #include <cassert>
 
 using namespace std;
@@ -13,11 +13,11 @@ int main()
 
 void getTested(){
     Tensor t_1_1(3);
-    Tensor t_1_2(2);
-    Tensor t_2_1(2,2);
-    Tensor t_2_2(2,2);
-    Tensor t_3_1(2,2,2);
-    Tensor t_3_2(2,2,2);
+    Tensor t_1_2(3);
+    Tensor t_2_1(4,4);
+    Tensor t_2_2(4,4);
+    Tensor t_3_1(3,3,3);
+    Tensor t_3_2(3,3,3);
 
     t_1_1.putElem(0,1);
     t_1_1.putElem(1,2);
@@ -62,4 +62,13 @@ void getTested(){
     Tensor t_substr = t_3_1 - t_3_2;
     assert(t_substr.getElem(1,1,0) == 0);
 
+    t_2_1.reshape(2,2);
+    assert(t_2_1.getElem(2,2)==3);
+
+    Tensor t_to_cut(5,5);
+    t_to_cut.putElem({1,1}, 3);
+    Tensor fin = t_to_cut.cut({0,2}, {0,2});
+    assert(fin.getElem(1,1)==3);
+
+    cout << "success";
 }
